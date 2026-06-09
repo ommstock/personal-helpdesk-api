2ee480d59dd742229c00df6d926909bf4637db6e
OmarMS <ommskara@gmail.com>
2026-06-09 17:13:37 -0500

    feat(logging): integrate centralized logging, using pino logging, CLS correlation IDs, and global exception handling

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  app.useLogger(app.get(Logger));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
+      transform: true, // Enables auto-transformation based on expected types
+      transformOptions: {
+        enableImplicitConversion: true, // Optionally allows implicit type conversion
+      },
     }),
   );
 
